7024e2e4c26731a858c8a4fadaa0a01ccdb56ce7
wenhu chen <wenhuchen@cs.ucsb.edu>
2021-06-17 20:53:14 -0700

    Update normalization.py

--- normalization.py
+++ normalization.py
@@ -1,1 +1,14 @@
 # Customized text normalization function
+def normalize(string):
+    string = string.replace('\n\n', ' <br> ')
+    string = string.replace('\n', ' <br> ')
+    string = re.sub('BULLET::::', '', string)
+    #string = re.sub('BULLET::::-', '', string)
+    string = re.sub('Section::::', '', string)
+    #pattern = '(' + '|'.join(puctuation) + ')'
+    string = re.sub(r'("|\')', '', string)
+    string = re.sub(r'(\,|\.|;|\?|!|:) ', r' \1 ', string)
+    string = re.sub(r'(\S)(|\)|\}|\]) ', r'\1 \2 ', string)
+    string = re.sub(r' (\(|\{|\[)(\S)', r' \1 \2', string)
+    string = re.sub(r'\s+', ' ', string)
+    return string
