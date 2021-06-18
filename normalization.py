493b1e7fc6c9f1bd2f491d70ed75fb25eedf8d2e
wenhu chen <wenhuchen@cs.ucsb.edu>
2021-06-18 13:47:11 -0700

    Update normalization.py

--- normalization.py
+++ normalization.py
@@ -3,13 +3,15 @@
 def normalize(string):
     string = string.replace('\n\n', ' <br> ')
     string = string.replace('\n', ' <br> ')
-    string = re.sub('BULLET::::', '', string)
-    #string = re.sub('BULLET::::-', '', string)
-    string = re.sub('Section::::', '', string)
-    #pattern = '(' + '|'.join(puctuation) + ')'
+    # Specifically for Wikipedia XML extraction
+    # string = re.sub('BULLET::::', '', string)
+    # string = re.sub('Section::::', '', string)
+    string = re.sub(' [^ ]+=[^ ]+ ', ' ', string)
     string = re.sub(r'("|\')', '', string)
+    string = re.sub(r'!{1,}(\w)', r' \1', string)
     string = re.sub(r'(\,|\.|;|\?|!|:)(\s|$)', r' \1\2', string)
     string = re.sub(r'(\)|\}|\])', r' \1 ', string)
     string = re.sub(r'(\(|\{|\[)', r' \1 ', string)
     string = re.sub(r'\s+', ' ', string)
     return string
+    
